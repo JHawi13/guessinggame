d7f0b2b4a435a6922b55247189a2143c31f25afe
JHawi13 <Joherwi@gmail.com>
2023-02-16 09:58:39 +0300

    A new variant of the game with limited tries

--- game.py
+++ game.py
@@ -5,14 +5,16 @@ print("Hello there",UserName,". are you ready to guess?")
 print(number)
 usernumber=eval(input("Guess a number:"))
 count=0
-while usernumber!= number:
-    print("Wrong")
-    usernumber=eval(input("Guess again"))
+while count<10:
+    if usernumber>number:
+        print("Your number is greater than the correct number")
+        usernumber=eval(input("Guess again:"))
+    elif usernumber<number:
+        print("Your number is lesser than the correct number")
+        usernumber=eval(input("Guess again:"))
+    elif usernumber==number:
+        print("You guessed right,you win!")
+        break
     count=count+1
-    if count>5:
-        print("You have guessed",count,"times.You can do better than that",UserName)
-    if count==7:
-        print("Here's a hint: The number is a real number less than 10")
+    print("you have",10-count,"tries remaining")
 
-if usernumber==number:
-    print("You guessed right")
